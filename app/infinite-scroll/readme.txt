875104f56aff84d5a3a2e74ef438b5057bc4f09b
Luyckx <ivo.luyckx@tokheimglobal.com>
2016-08-09 19:26:08 +0200

    Revert "use infinite-scroll source code instead of npm package"
    This reverts commit 0ef10d1b442d672e27b6bb9693f4c95af9d5a1d9: the code
    found on the internet does not seem to work as expected.

--- app/infinite-scroll/readme.txt
+++
@@ -1,8 +0,0 @@
-source:    https://github.com/orizens/angular2-infinite-scroll
-timestamp: 08/08/3026 20h00
-
-I could not get the npm install working:
-- the install works,
-- but the include in typescript files results in run-time-errors.
-As such I did include the source code here
-
--- app/process-group-list.component.ts
+++ app/process-group-list.component.ts
@@ -5,15 +5,14 @@ import { Subject }                     from 'rxjs/Subject';
 
 import 'rxjs/add/operator/do';
 
-import { InfiniteScroll }              from './infinite-scroll/infinite-scroll';
-
 import { ProcessGroup }                from './process-group';
 import { ProcessGroupDetailComponent } from './process-group-detail.component';
 import { ProcessGroupService }         from './process-group.service';
+import { IsVisibleDirective}           from './is-visible.directive';
 
 @Component({
   selector: 'process-group-list',
-  directives: [ProcessGroupDetailComponent, InfiniteScroll ],
+  directives: [ProcessGroupDetailComponent, IsVisibleDirective],
   templateUrl: 'app/process-group-list.component.html',
 })
 export class ProcessGroupListComponent implements OnInit { 
@@ -30,15 +29,17 @@ export class ProcessGroupListComponent implements OnInit {
   }
 
   private getProcessGroups() {
-	  this.processGroups = this.processGroupsService.getProcessGroups();
+	  this.processGroups = this.processGroupsService.getProcessGroups()
+      .do(e => ++this.requestCheck); //inform the scroll-event-directive that items have been loaded 
+                                     //which actually occurs before the drawing --> so probably this will always trigger a second load
   }
 
   public onNext() {
     this.processGroupsService.loadProcessGroups();
   }
 
-  public onScroll() {
-    console.log("onScroll");
+  public isVisible() {
+    console.log("isVisible caugth");
     this.processGroupsService.loadProcessGroups();
-  }
+  }  
 }
